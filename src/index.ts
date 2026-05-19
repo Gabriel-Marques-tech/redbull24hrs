e2498e450936115004997d496afdaeecef0d606c
Gabriel Marques <gabriel.marques@sou.inteli.edu.br>
2026-05-18 21:01:30 -0300

    feat: Adiciona função bootstrap em index.ts | #90
    - Função bootstrap usada para preparar e testar conexões e necessidades prévias do servidor
    - Tudo que for necessario para o servidor inicializar e funcionar deve estar contido e verificado na função bootstrap
    
    Para funcionar adequadamente um server tem requesitos, a função bootstrap tem a finalidade de testar, valdiar e iniciar tudo que é necessario para o servidor iniciar

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import express from "express";
+import { pool } from "./database/connection";
+import { config } from "dotenv";
+
+config();
+const port = Number(process.env.SERVER_PORT);
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => console.log("On-line Server"));
+async function bootstrap() {
+	try {
+		const client = await pool.connect();
+		client.release();
+		console.log("Conexão com o banco estabelecida");
+
+		app.listen(port, () => console.log(`On-line server on ${port} port`));
+	} catch (error) {
+		console.error("Erro ao conectar com o banco:", error);
+		process.exit(1);
+	}
+}
+
+bootstrap();
