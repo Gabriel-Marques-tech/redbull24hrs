0fa17dc1143992bc075a7d4e92620fd0f0a46455
Gabriel Marques <gabriel.marques@sou.inteli.edu.br>
2026-05-20 15:33:57 -0300

    feat: Adiciona função de registro de usuario | #99
    - Cria controller, service, repository e routes de autenticação
    - Adiciona função de login
    - Adiciona service de encriptamento da senha
    
    Para atender os requesitos não funcionais de SEGURANÇA, realizamos por meio de autenticação o controle de acesso

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,10 @@
 import express, { Router } from "express";
+import AuthController from "../controllers/authController";
 
 const router: Router = express.Router();
 
-router.get("", () => console.log("teste"));
+router.get("/teste", () => console.log("teste"));
+
+router.post("/register", AuthController.registerUser);
 
 export default router;
