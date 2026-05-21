c963f5e71940ef474f34030746591e12687bcf45
Gabriel Marques <gabriel.marques@sou.inteli.edu.br>
2026-05-20 22:38:47 -0300

    refactor: Atualiza service para receber a tipagem correta | #99
    - Tipa adequadamente funções de registro
    - Cria tipos personalizados para os Managers
    
    Para manter a funcionalidade total dos testes e facil manutenção futura

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,19 +1,24 @@
 import UserRepository from "../repositories/userRepository";
 import bcrypt from "bcrypt";
+import { Manager } from "../types/user.types";
 
-const registerUser = async (name: string, email: string, password: string) => {
-	if (!name || !email || !password) return null;
-	else {
-		const saltRounds: number = 10;
-		const hashedPassword: string = await bcrypt.hash(password, saltRounds);
-		const user = UserRepository.registerAuditor(
-			name,
-			email,
-			hashedPassword,
-		);
+const registerManager = async (
+  name: string,
+  email: string,
+  password: string,
+): Promise<Manager | null> => {
+  if (!name || !email || !password) return null;
+  else {
+    const saltRounds: number = 10;
+    const hashedPassword: string = await bcrypt.hash(password, saltRounds);
+    const user = await UserRepository.registerManager(
+      name,
+      email,
+      hashedPassword,
+    );
 
-		return user;
-	}
+    return user;
+  }
 };
 
-export default { registerUser };
+export default { registerManager };
