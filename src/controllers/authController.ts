b422ae07c3debbd8ff1e02e6a676cdf098aafb5c
Gabriel Marques <gabriel.marques@sou.inteli.edu.br>
2026-05-20 22:38:47 -0300

    refactor: Atualiza service para receber a tipagem correta | #99
    - Tipa adequadamente funções de registro
    - Cria tipos personalizados para os Managers
    
    Para manter a funcionalidade total dos testes e facil manutenção futura

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,24 +1,24 @@
 import { Response, Request } from "express";
 import AuthService from "../services/authService";
 
-const registerUser = async (req: Request, res: Response): Promise<void> => {
-	const { name, email, password } = req.body;
-	const status = await AuthService.registerUser(name, email, password);
-	if (!status) {
-		res.status(404).json({ error: "Impossivel cadastrar usuario" });
-	} else {
-		res.status(200).json(status);
-	}
+const registerManager = async (req: Request, res: Response): Promise<void> => {
+  const { name, email, password } = req.body;
+  const status = await AuthService.registerManager(name, email, password);
+  if (!status) {
+    res.status(404).json({ error: "Impossivel cadastrar usuario" });
+  } else {
+    res.status(200).json(status);
+  }
 };
 
-const loginUser = async (req: Request, res: Response): Promise<void> => {
-	const { email, password } = req.body;
-	const status = await AuthService.loginUser(email, password);
-	if (!status) {
-		res.status(404).json({ error: "Usuario não encontrado" });
-	} else {
-		res.status(200).json(status);
-	}
-};
+/*const loginUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+  const status = await AuthService.loginUser(email, password);
+  if (!status) {
+    res.status(404).json({ error: "Usuario não encontrado" });
+  } else {
+    res.status(200).json(status);
+  }
+}; */
 
-export default { registerUser, loginUser };
+export default { registerManager /*loginUser*/ };
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,37 +1,38 @@
 import { pool } from "../database/connection";
+import { Manager } from "../types/user.types";
 
 const registerManager = async (
-	name: string,
-	email: string,
-	password: string,
+  name: string,
+  email: string,
+  password: string,
 ) => {
-	const user = pool.query(
-		`INSERT INTO managers (name, password, email) 
+  const result = await pool.query(
+    `INSERT INTO managers (name, password, email) 
 		VALUES ($1, $2, $3)
 		RETURNING id, name, email`,
-		[name, password, email],
-	);
+    [name, password, email],
+  );
 
-	return user;
+  return result.rows[0];
 };
 
 const registerAuditor = async (
-	name: string,
-	email: string,
-	password: string,
-) => {
-	const user = pool.query(
-		`INSERT INTO managers (name, password, email) 
+  name: string,
+  email: string,
+  password: string,
+): Promise<Manager> => {
+  const result = await pool.query(
+    `INSERT INTO managers (name, password, email) 
 		VALUES ($1, $2, $3)
 		RETURNING id, name, email`,
-		[name, password, email],
-	);
+    [name, password, email],
+  );
 
-	return user;
+  return result.rows[0];
 };
 
 const loginUser = async (email: string, password: string) => {
-	const user = pool.query(`SELECT id, password, email FROM managers`);
+  const user = await pool.query(`SELECT id, password, email FROM managers`);
 };
 
-export default { registerAuditor };
+export default { registerManager };
