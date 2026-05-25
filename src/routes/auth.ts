91b99b464ad3f9cbf26f8e8c11e0957b84404ab2
Gabriel Marques <gabriel.marques@sou.inteli.edu.br>
2026-05-25 15:23:45 -0300

    feat: Adiciona fluxo MVC de autenticação completo | #99
    - Routes de autenticação definidas com cada endpoint
    -
    - Controller de autenticação manipula as requisições de cada route e processa redirecionando para o service
    - Services de autenticação validam e executam regras de negócios
    - repository de usuario contata o banco de dados e executa CRUD associado adequadamente
    
    Fluxo criado para ser modular e permitir implementação futura e manutenção com eficiencia

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,17 @@
 import express, { Router } from "express";
 import AuthController from "../controllers/authController";
+import AuthMiddleware from "../middlewares/authMiddleware";
 
 const router: Router = express.Router();
 
-router.post("/register", AuthController.registerUser);
-
+router.post("/register/manager", AuthController.registerManager);
+router.post("/register/auditor", AuthController.registerAuditor);
 router.post("/login", AuthController.loginUser);
+router.post("/refresh", AuthController.refreshToken);
+router.post("/logout", AuthController.logout);
+
+router.get("/me", AuthMiddleware.requireAuth, (req, res) => {
+  res.status(200).json(req.user);
+});
 
 export default router;
