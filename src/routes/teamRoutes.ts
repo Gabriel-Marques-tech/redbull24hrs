5e4d72859f6b0123c9627fd52262b3b19e3f2ca5
pietro.lucas <pietro.lucas@sou.inteli.edu.br>
2026-06-17 10:12:44 -0300

    feat: atualiza route teamRoutes.ts | #255

--- src/routes/teamRoutes.ts
+++ src/routes/teamRoutes.ts
@@ -1,7 +1,9 @@
 import express, { Router } from "express";
+import multer from "multer";
 import { teamController } from "../controllers/teamController";
 
 const router: Router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", teamController.getTeams);
 router.post("/", teamController.createTeam);
@@ -10,9 +12,9 @@ router.patch("/:id", teamController.updateTeam);
 router.delete("/:id", teamController.deleteTeam);
 
 router.get("/:teamId/athletes", teamController.getAthletes);
-router.post("/:teamId/athletes", teamController.createAthlete);
+router.post("/:teamId/athletes", upload.single("photo"), teamController.createAthlete);
 router.get("/:teamId/athletes/:id", teamController.getAthlete);
-router.patch("/:teamId/athletes/:id", teamController.updateAthlete);
+router.patch("/:teamId/athletes/:id", upload.single("photo"), teamController.updateAthlete);
 router.delete("/:teamId/athletes/:id", teamController.deleteAthlete);
 
 export default router;
